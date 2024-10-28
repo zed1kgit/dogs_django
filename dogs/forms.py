520a48209e0b118bf2ac533aa37799179d9ac173
Ilya Seleznev <zedplay.zp@gmail.com>
2024-10-29 02:23:31 +0300

    Added class StyleFormMixin

--- dogs/forms.py
+++ dogs/forms.py
@@ -2,8 +2,14 @@ from django import forms
 
 from dogs.models import Dog
 
+class StyleFormMixin:
+    def __init__(self,*args , **kwargs):
+        super().__init__(*args , **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
-class DogForm(forms.ModelForm):
+
+class DogForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Dog
         fields = '__all__'
