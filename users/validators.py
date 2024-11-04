73ca8c5728c650bb6cc224e9a59245f5bcade492
Ilya Seleznev <zedplay.zp@gmail.com>
2024-11-05 01:05:39 +0300

    Added multi-language errors in validator

--- users/validators.py
+++ users/validators.py
@@ -1,10 +1,29 @@
 import re
+
+from django.conf import settings
 from django.core.exceptions import ValidationError
 
 
 def validate_password(field):
     pattern = re.compile('^[a-zA-Z0-9]+$')
+    language = settings.LANGUAGE_CODE
+    error_messages = [
+        {
+            'ru-ru': 'Пароль должен содержать буквы латинского алфавита и цифры!',
+            'en-us': 'Password must contain A-Z, a-z, and 0-9 characters!',
+        },
+        {
+            'ru-ru': 'Длина пароля должна быть от 6 до 12 символов!',
+            'en-us': 'Password length must be between 6 and 12 characters!',
+        }
+    ]
     if not pattern.match(field):
-        raise ValidationError('Пароль должен содержать A-Z, a-z буквы и 0-9 цифры')
+        raise ValidationError(
+            error_messages[0][language],
+            code=error_messages[0][language]
+        )
     if not 6 <= len(field) <= 12:
-        raise ValidationError('Длинна пароля должна быть от 6 до 12 символов')
+        raise ValidationError(
+            error_messages[1][language],
+            code=error_messages[1][language]
+        )
