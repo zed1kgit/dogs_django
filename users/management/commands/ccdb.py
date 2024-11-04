e5423c4544a05edbb03e9122149a605ae4e70698
Ilya Seleznev <zedplay.zp@gmail.com>
2024-11-05 00:43:37 +0300

    Updated ccdb command

--- users/management/commands/ccdb.py
+++ users/management/commands/ccdb.py
@@ -13,11 +13,15 @@ class Command(BaseCommand):
                                        PWD={PASSWORD}'''
         try:
             conn = pyodbc.connect(ConnectionString)
-            conn.autocommit = True
-            conn.execute(fr"CREATE DATABASE {DATABASE};")
         except pyodbc.ProgrammingError as ex:
             print(ex)
         else:
-            print(f"DB {DATABASE} created")
+            conn.autocommit = True
+            try:
+                conn.execute(fr"CREATE DATABASE {DATABASE};")
+            except pyodbc.ProgrammingError as ex:
+                print(ex)
+            else:
+                print(f"DB {DATABASE} created")
         finally:
             conn.close()
