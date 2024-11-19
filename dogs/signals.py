8a72da67e8fe51319c5d9ed614dba155335e2a64
Ilya Seleznev <zedplay.zp@gmail.com>
2024-11-20 00:24:25 +0300

    Added dogs signals docstrings

--- dogs/signals.py
+++ dogs/signals.py
@@ -7,5 +7,15 @@ from dogs.services import send_congratulation_mail_task
 
 @receiver(post_save, sender=Dog)
 def congratulation_mail(sender, instance, **kwargs):
+    """
+    Отправка уведомления о достижении определённого числа просмотров собакой.
+
+    Аргументы:
+       instance (Dog): Инстанция модели собаки, владельца которой необходимо уведомить.
+       kwargs: Параметры, переданные сигналом.
+
+    Действие:
+       Если количество просмотров кратно 100, отправляется уведомление владельцу о достижении данного порога.
+    """
     if instance.view_count % 100 == 0 and instance.owner:
         send_congratulation_mail_task.delay(instance.owner.email, instance.id, instance.view_count)
