c537f382c41e58e85a5c13775d28fe5dfe3d3806
Ilya Seleznev <zedplay.zp@gmail.com>
2024-11-20 01:02:33 +0300

    Update dogs views docstrings

--- dogs/views.py
+++ dogs/views.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import PermissionDenied
 from django.forms import inlineformset_factory
-from django.http import Http404, HttpResponseForbidden
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
@@ -9,12 +9,23 @@ from django.db.models import Q
 
 from dogs.models import Category, Dog, Parent
 from dogs.forms import DogForm, ParentForm, DogAdminForm
-from dogs.services import send_congratulation_mail
 from users.models import UserRoles
 
 
 def index(request):
-    """Рендер главной страницы"""
+    """
+    Отображение главной страницы сайта.
+
+    Параметры:
+        request (HttpRequest): Запрос от клиента.
+
+    Контекст:
+        category_object_list (QuerySet): Список первых трех записей из таблицы категорий.
+        title (str): Заголовок страницы.
+
+    Возвращает:
+        HttpResponse: Ответ с рендером главной страницы.
+    """
     context = {
         'category_object_list': Category.objects.all()[:3],
         'title': 'Главная'
@@ -22,7 +33,15 @@ def index(request):
     return render(request, 'dogs/index.html', context)
 
 
-class CategoryListView(ListView, LoginRequiredMixin):
+class CategoryListView(LoginRequiredMixin, ListView):
+    """
+    Представление списка всех категорий.
+
+    Атрибуты:
+        model (Model): Модель категории.
+        extra_context (dict): Дополнительный контекст для шаблона.
+        template_name (str): Имя файла шаблона.
+    """
     model = Category
     extra_context = {
         'title': 'Все наши породы'
@@ -31,7 +50,21 @@ class CategoryListView(ListView, LoginRequiredMixin):
 
 
 def category_dogs(request, pk):
-    """Рендер страницы с собаками определенной породы"""
+    """
+    Отображение страницы с собаками определенной породы.
+
+    Параметры:
+        request (HttpRequest): Запрос от клиента.
+        pk (int): Первичный ключ категории.
+
+    Контекст:
+        object_list (QuerySet): Список собак выбранной категории.
+        title (str): Заголовок страницы.
+        category_pk (int): Идентификатор категории.
+
+    Возвращает:
+        HttpResponse: Ответ с рендером страницы с собаками определенной породы.
+    """
     category_item = Category.objects.get(pk=pk)
     context = {
         'object_list': Dog.objects.filter(category_id=pk),
@@ -42,6 +75,18 @@ def category_dogs(request, pk):
 
 
 class DogListView(ListView):
+    """
+    Представление списка активных собак.
+
+    Атрибуты:
+        model (Model): Модель собаки.
+        paginate_by (int): Количество объектов на странице.
+        extra_context (dict): Дополнительный контекст для шаблона.
+        template_name (str): Имя файла шаблона.
+
+    Методы:
+        get_queryset(): Переопределенный метод получения QuerySet, который фильтрует активные собаки.
+    """
     model = Dog
     paginate_by = 6
     extra_context = {
@@ -50,12 +95,29 @@ class DogListView(ListView):
     template_name = 'dogs/dogs.html'
 
     def get_queryset(self):
+        """
+        Получение QuerySet с активными собаками.
+
+        Возвращает:
+            QuerySet: Фильтрованный список активных собак.
+        """
         queryset = super().get_queryset()
         queryset = queryset.filter(is_active=True)
         return queryset
 
 
-class DogDeactivateListView(ListView, LoginRequiredMixin):
+class DogDeactivateListView(LoginRequiredMixin, ListView):
+    """
+    Представление списка неактивных собак.
+
+    Атрибуты:
+        model (Model): Модель собаки.
+        extra_context (dict): Дополнительный контекст для шаблона.
+        template_name (str): Имя файла шаблона.
+
+    Методы:
+        get_queryset(): Переопределенный метод получения QuerySet, который фильтрует неактивные собаки в зависимости от роли пользователя.
+    """
     model = Dog
     extra_context = {
         'title': 'Неактивные собаки'
@@ -63,6 +125,12 @@ class DogDeactivateListView(ListView, LoginRequiredMixin):
     template_name = 'dogs/dogs.html'
 
     def get_queryset(self):
+        """
+        Получение QuerySet с неактивными собаками.
+
+        Возвращает:
+            QuerySet: Фильтрованный список неактивных собак в зависимости от роли пользователя.
+        """
         queryset = super().get_queryset()
         if self.request.user.role in [UserRoles.MODERATOR, UserRoles.ADMIN]:
             queryset = queryset.filter(is_active=False)
@@ -71,11 +139,31 @@ class DogDeactivateListView(ListView, LoginRequiredMixin):
         return queryset
 
 
-class DogSearchListView(ListView, LoginRequiredMixin):
+class DogSearchListView(LoginRequiredMixin, ListView):
+    """
+    Представление списка результатов поиска собак.
+
+    Атрибуты:
+        model (Model): Модель собаки.
+        template_name (str): Имя файла шаблона.
+
+    Методы:
+        get_queryset(): Переопределенный метод получения QuerySet, который фильтрует результаты поиска.
+        get_context_data(**kwargs): Переопределенный метод добавления дополнительного контекста для шаблона.
+    """
     model = Dog
     template_name = 'dogs/dogs.html'
 
     def get_queryset(self):
+        """
+        Получение QuerySet с результатами поиска собак.
+
+        Параметры:
+            q (str): Строка запроса.
+
+        Возвращает:
+            QuerySet: Фильтрованный список собак, соответствующих запросу.
+        """
         query = self.request.GET.get('q')
         object_list = Dog.objects.filter(
             Q(name__icontains=query), is_active=True,
@@ -83,16 +171,45 @@ class DogSearchListView(ListView, LoginRequiredMixin):
         return object_list
 
     def get_context_data(self, **kwargs):
+        """
+        Добавление дополнительного контекста для шаблона.
+
+        Параметры:
+            **kwargs: Аргументы ключевого слова.
+
+        Возвращает:
+            dict: Обновленный контекст для шаблона.
+        """
         context = super().get_context_data(**kwargs)
         context['title'] = f'Поиск собаки: {self.request.GET.get("q")}'
         return context
 
 
-class CategorySearchListView(ListView, LoginRequiredMixin):
+class CategorySearchListView(LoginRequiredMixin, ListView):
+    """
+    Представление списка результатов поиска пород собак.
+
+    Атрибуты:
+        model (Model): Модель категории.
+        template_name (str): Имя файла шаблона.
+
+    Методы:
+        get_queryset(): Переопределенный метод получения QuerySet, который фильтрует результаты поиска.
+        get_context_data(**kwargs): Переопределенный метод добавления дополнительного контекста для шаблона.
+    """
     model = Category
     template_name = 'dogs/categories.html'
 
     def get_queryset(self):
+        """
+        Получение QuerySet с результатами поиска пород собак.
+
+        Параметры:
+            q (str): Строка запроса.
+
+        Возвращает:
+            QuerySet: Фильтрованный список категорий, соответствующих запросу.
+        """
         query = self.request.GET.get('q')
         object_list = Category.objects.filter(
             Q(name__icontains=query),
@@ -100,18 +217,51 @@ class CategorySearchListView(ListView, LoginRequiredMixin):
         return object_list
 
     def get_context_data(self, **kwargs):
+        """
+        Добавление дополнительного контекста для шаблона.
+
+        Параметры:
+            **kwargs: Аргументы ключевого слова.
+
+        Возвращает:
+            dict: Обновленный контекст для шаблона.
+        """
         context = super().get_context_data(**kwargs)
         context['title'] = f'Поиск породы: {self.request.GET.get("q")}'
         return context
 
 
 class DogCreateView(LoginRequiredMixin, CreateView):
+    """
+    Представление создания новой собаки.
+
+    Атрибуты:
+        model (Model): Модель собаки.
+        form_class (Form): Форма для создания собаки.
+        template_name (str): Имя файла шаблона.
+        success_url (str): URL для перенаправления после успешного сохранения.
+
+    Методы:
+        form_valid(form): Переопределенный метод обработки формы при успешной валидации.
+    """
     model = Dog
     form_class = DogForm
     template_name = 'dogs/create_update.html'
     success_url = reverse_lazy('dogs:list_dogs')
 
     def form_valid(self, form):
+        """
+        Обработка формы при успешной валидации.
+
+        Параметры:
+            form (DogForm): Форма для создания собаки.
+
+        Исключения:
+            PermissionDenied: Если пользователь не является пользователем.
+
+        Возвращает:
+            HttpResponseRedirect: Перенаправление на страницу успеха.
+        """
         if self.request.user.role != UserRoles.USER:
             raise PermissionDenied()
         self.object = form.save()
@@ -121,10 +271,32 @@ class DogCreateView(LoginRequiredMixin, CreateView):
 
 
 class DogDetailView(DetailView):
+    """
+    Представление детальной информации о собаке.
+
+    Атрибуты:
+        model (Model): Модель собаки.
+        template_name (str): Имя файла шаблона.
+
+    Методы:
+        get_object(queryset=None): Переопределенный метод получения объекта.
+    """
     model = Dog
     template_name = 'dogs/detail.html'
 
     def get_object(self, queryset=None):
+        """
+        Получение объекта собаки.
+
+        Параметры:
+            queryset (QuerySet): Набор запросов.
+
+        Возвращает:
+            Dog: Объект собаки.
+
+        Исключения:
+            Http404: Если объект не найден.
+        """
         self.object = super().get_object(queryset=queryset)
         if self.request.user != self.object.owner:
             self.object.view_count += 1
@@ -133,19 +305,60 @@ class DogDetailView(DetailView):
 
 
 class DogUpdateView(LoginRequiredMixin, UpdateView):
+    """
+    Представление обновления информации о собаке.
+
+    Атрибуты:
+        model (Model): Модель собаки.
+        template_name (str): Имя файла шаблона.
+
+    Методы:
+        get_success_url(): Получение URL для перенаправления после успешного обновления.
+        get_object(queryset=None): Переопределенный метод получения объекта.
+        get_context_data(**kwargs): Переопределенный метод добавления дополнительного контекста для шаблона.
+        form_valid(form): Переопределенный метод обработки формы при успешной валидации.
+        get_form_class(): Получение класса формы в зависимости от роли пользователя.
+    """
     model = Dog
     template_name = 'dogs/create_update.html'
 
     def get_success_url(self):
+        """
+        Получение URL для перенаправления после успешного обновления.
+
+        Возвращает:
+            str: URL для перенаправления.
+        """
         return reverse_lazy('dogs:detail_dog', kwargs={'pk': self.object.pk})
 
     def get_object(self, queryset=None):
+        """
+        Получение объекта собаки.
+
+        Параметры:
+            queryset (QuerySet): Набор запросов.
+
+        Возвращает:
+            Dog: Объект собаки.
+
+        Исключения:
+            PermissionDenied: Если пользователь не является владельцем или администратором.
+        """
         self.object = super().get_object(queryset=queryset)
         if self.request.user != self.object.owner and self.request.user.role != UserRoles.ADMIN:
             raise PermissionDenied()
         return self.object
 
     def get_context_data(self, **kwargs):
+        """
+        Добавление дополнительного контекста для шаблона.
+
+        Параметры:
+            **kwargs: Аргументы ключевого слова.
+
+        Возвращает:
+            dict: Обновленный контекст для шаблона.
+        """
         context_data = super().get_context_data(**kwargs)
         ParentFormset = inlineformset_factory(Dog, Parent, form=ParentForm, extra=1)
         if self.request.method == 'POST':
@@ -156,6 +369,15 @@ class DogUpdateView(LoginRequiredMixin, UpdateView):
         return context_data
 
     def form_valid(self, form):
+        """
+        Обработка формы при успешной валидации.
+
+        Параметры:
+            form (DogForm): Форма для обновления собаки.
+
+        Возвращает:
+            HttpResponseRedirect: Перенаправление на страницу успеха.
+        """
         context_data = self.get_context_data()
         formset = context_data['formset']
         self.object = form.save()
@@ -165,6 +387,12 @@ class DogUpdateView(LoginRequiredMixin, UpdateView):
         return super().form_valid(form)
 
     def get_form_class(self):
+        """
+        Получение класса формы в зависимости от роли пользователя.
+
+        Возвращает:
+            Form: Класс формы для обновления собаки.
+        """
         if self.request.user.role == UserRoles.ADMIN:
             form_class = DogAdminForm
         else:
@@ -173,12 +401,36 @@ class DogUpdateView(LoginRequiredMixin, UpdateView):
 
 
 class DogDeleteView(LoginRequiredMixin, DeleteView, PermissionRequiredMixin):
+    """
+    Представление удаления собаки.
+
+    Атрибуты:
+        model (Model): Модель собаки.
+        template_name (str): Имя файла шаблона.
+        success_url (str): URL для перенаправления после успешного удаления.
+        permission_required (tuple): Требуемые разрешения.
+
+    Методы:
+        get_object(queryset=None): Переопределенный метод получения объекта.
+    """
     model = Dog
     template_name = 'dogs/delete.html'
     success_url = reverse_lazy('dogs:list_dogs')
     permission_required = ('dogs.delete_dog',)
 
     def get_object(self, queryset=None):
+        """
+        Получение объекта собаки.
+
+        Параметры:
+            queryset (QuerySet): Набор запросов.
+
+        Возвращает:
+            Dog: Объект собаки.
+
+        Исключения:
+            Http404: Если объект не найден.
+        """
         self.object = super().get_object(queryset=queryset)
         if self.request.user != self.object.owner and not self.request.user.is_staff:
             raise Http404
@@ -186,6 +438,19 @@ class DogDeleteView(LoginRequiredMixin, DeleteView, PermissionRequiredMixin):
 
 
 def dog_toggle_activity(request, pk):
+    """
+    Переключение активности собаки.
+
+    Параметры:
+        request (HttpRequest): Запрос от клиента.
+        pk (int): Первичный ключ собаки.
+
+    Возвращает:
+        HttpResponseRedirect: Перенаправление на страницу списка собак.
+
+    Исключения:
+        Http404: Если собака не найдена.
+    """
     dog_item = get_object_or_404(Dog, pk=pk)
     if dog_item.is_active:
         dog_item.is_active = False
