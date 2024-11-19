32292b298e275cb4f026c1eb2769b1d5b8b0514b
Ilya Seleznev <zedplay.zp@gmail.com>
2024-11-19 23:27:07 +0300

    Added user models docstrings

--- users/models.py
+++ users/models.py
@@ -4,13 +4,40 @@ from django.utils.translation import gettext_lazy as _
 
 NULLABLE = {'blank': True, 'null': True}
 
+
 class UserRoles(models.TextChoices):
+    """
+    Класс для выбора ролей пользователя.
+    """
     ADMIN = 'admin', _('admin')
     MODERATOR = 'moderator', _('moderator')
     USER = 'user', _('user')
 
 
 class User(AbstractUser):
+    """
+    Модель пользователя, расширяющая стандартную модель Django.
+
+    Поля:
+        username (None): Поле отключено.
+        email (EmailField): Уникальный адрес электронной почты.
+        role (CharField): Роль пользователя, выбирается из вариантов UserRoles.
+        first_name (CharField): Имя пользователя.
+        last_name (CharField): Фамилия пользователя.
+        phone (CharField): Номер телефона (необязательное поле).
+        telegram (CharField): Никнейм в Telegram (необязательное поле).
+        avatar (ImageField): Аватар пользователя (необязательное поле).
+        is_active (BooleanField): Признак активности пользователя.
+
+    Методы:
+        __str__(): Строковое представление объекта пользователя.
+
+    Метакласс:
+        verbose_name: Название модели в единственном числе.
+        verbose_name_plural: Название модели во множественном числе.
+        ordering: Сортировка по полю id.
+    """
+
     username = None
     email = models.EmailField(unique=True, verbose_name='email')
     role = models.CharField(max_length=9, choices=UserRoles.choices, default=UserRoles.USER)
@@ -25,6 +52,9 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
 
     def __str__(self):
+        """
+        Возвращает строку, представляющую объект пользователя.
+        """
         return f'{self.email}'
 
     class Meta:
