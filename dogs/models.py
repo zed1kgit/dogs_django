74e19762734775daf294dfe2cfc0f073286fc5e3
Ilya Seleznev <zedplay.zp@gmail.com>
2024-11-20 00:14:58 +0300

    Updated dogs models docstrings

--- dogs/models.py
+++ dogs/models.py
@@ -5,7 +5,20 @@ from users.models import NULLABLE
 
 
 class Category(models.Model):
-    """Класс породы собак"""
+    """
+    Модель категории (например, порода собак). Включает название и описание.
+
+    Атрибуты:
+        name (CharField): Название категории.
+        description (CharField): Описание категории.
+
+    Методы:
+        __str__ (str): Возвращает название категории.
+
+    Метакласс:
+        verbose_name (str): Название модели в единственном числе.
+        verbose_name_plural (str): Название модели во множественном числе.
+    """
     name = models.CharField(max_length=100, verbose_name='breed')
     description = models.CharField(max_length=1000, verbose_name='description')
 
@@ -18,7 +31,25 @@ class Category(models.Model):
 
 
 class Dog(models.Model):
-    """Класс собаки"""
+    """
+    Модель собаки. Включает имя, категорию, фото, дату рождения, активность, количество просмотров и владельца.
+
+    Атрибуты:
+        name (CharField): Имя собаки.
+        category (ForeignKey): Категория (например, порода собак).
+        photo (ImageField): Фотография собаки.
+        birth_date (DateField): Дата рождения собаки.
+        is_active (BooleanField): Активен ли питомец.
+        view_count (PositiveIntegerField): Количество просмотров.
+        owner (ForeignKey): Владелец собаки.
+
+    Методы:
+        __str__ (str): Возвращает имя собаки вместе с категорией.
+
+    Метакласс:
+        verbose_name (str): Название модели в единственном числе.
+        verbose_name_plural (str): Название модели во множественном числе.
+    """
     name = models.CharField(max_length=250, verbose_name='dog_name')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='breed')
     photo = models.ImageField(upload_to='dogs/', verbose_name='image', **NULLABLE)
@@ -36,6 +67,22 @@ class Dog(models.Model):
         verbose_name_plural = 'dogs'
 
 class Parent(models.Model):
+    """
+    Родительская модель. Связана с моделью собаки. Включает имя, категорию, дату рождения и ссылку на собаку.
+
+    Атрибуты:
+        name (Charify): Имя родителя.
+        category (ForeignKey): Категория (например, порода собак).
+        birth_date (DateField): Дата рождения родителя.
+        dog (ForeignKey): Собака, связанная с родителем.
+
+    Методы:
+        __str__ (str): Возвращает имя родителя вместе с категорией.
+
+    Метакласс:
+        verbose_name (str): Название модели в единственном числе.
+        verbose_name_plural (str): Название модели во множественном числе.
+    """
     dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
     name = models.CharField(max_length=250, verbose_name='dog_name')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='breed')
