72b850e872c04bacd3e23283d3aea8852c53b70a
Ilya Seleznev <zedplay.zp@gmail.com>
2024-11-19 23:38:46 +0300

    Added reviews utils docstrings

--- reviews/utils.py
+++ reviews/utils.py
@@ -2,4 +2,14 @@ import string
 import random
 
 def slug_generator(size=20, chars=string.ascii_letters + string.digits):
+    """
+    Генерирует уникальный слаг (случайная строка символов).
+
+    Аргументы:
+        size (int): Длина генерируемого слога. По умолчанию 20.
+        chars (str): Символы, используемые для генерации слага. По умолчанию включает буквы латинского алфавита и цифры.
+
+    Возвращает:
+        str: Сгенерированный слаг заданной длины.
+    """
     return ''.join(random.choice(chars) for _ in range(size))
