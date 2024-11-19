f810a4830d8a68f52e6f9d4b6e9588467bf5ce24
Ilya Seleznev <zedplay.zp@gmail.com>
2024-11-20 00:17:32 +0300

    Added dogs services docstrings

--- dogs/services.py
+++ dogs/services.py
@@ -8,6 +8,15 @@ from dogs.models import Category, Dog
 
 
 def get_categories_cache():
+    """
+    Получение списка категорий из кеша. Если кеш включен, то возвращается кэшированный список категорий, иначе создается новый список.
+
+    Параметры:
+        key (str): Ключ для доступа к списку категорий в кеше.
+
+    Возвраты:
+        list: Список категорий.
+    """
     if settings.CACHE_ENABLED:
         key = 'category_list'
         category_list = cache.get(key)
@@ -21,6 +30,17 @@ def get_categories_cache():
 
 
 def send_congratulation_mail(email, obj, count):
+    """
+    Отправка поздравления по поводу достижения определенного количества просмотров.
+
+    Параметры:
+        email (str): Электронный адрес получателя.
+        obj (Dog): Объект собаки, достигшей определенного количества просмотров.
+        count (int): Количество достигнутых просмотров.
+
+    Возврат:
+        None: Функция выполняет отправку сообщения.
+    """
     send_mail(
         subject=f'Поздравляем {count} просмотров!!',
         message=f'Ваша собака - {obj.name}, преодолела {count} просмотров!!',
@@ -31,5 +51,16 @@ def send_congratulation_mail(email, obj, count):
 
 @shared_task
 def send_congratulation_mail_task(email, obj_id, count):
+    """
+    Отправка поздравления по достижении определенного количества просмотров через задачу Celery.
+
+    Параметры:
+        email (str): Электронный адрес получателя.
+        obj_id (int): ID объекта собаки.
+        count (int): Количество достигнутых просмотров.
+
+    Возврат:
+        None: Задача выполняется асинхронно.
+    """
     obj = get_object_or_404(Dog, id=obj_id)
     return send_congratulation_mail(email, obj, count)
