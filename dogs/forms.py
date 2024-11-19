498b4f5dae87852fd098b2399886bfafc5e851e8
Ilya Seleznev <zedplay.zp@gmail.com>
2024-11-20 00:07:12 +0300

    Added dogs forms docstrings

--- dogs/forms.py
+++ dogs/forms.py
@@ -6,18 +6,51 @@ from dogs.models import Dog, Parent
 
 
 class StyleFormMixin:
+    """
+    Миксин для добавления стиля к полям форм.
+    """
+
     def __init__(self, *args, **kwargs):
+        """
+        Инициализация миксина. Добавляет класс 'form-control' ко всем полям формы.
+
+        Аргументы:
+            args: Позиционные аргументы.
+            kwargs: Именованные аргументы.
+        """
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
 
 
 class DogForm(StyleFormMixin, forms.ModelForm):
+    """
+    Форма для создания и редактирования информации о собаках.
+
+    Атрибуты:
+        model (Dog): Модель собак.
+        exclude (tuple): Поля, исключаемые из формы.
+
+    Методы:
+        clean_birth_date(self): Валидация даты рождения собаки.
+    """
     class Meta:
         model = Dog
         exclude = ('owner', 'is_active', 'view_count')
 
     def clean_birth_date(self):
+        """
+        Валидация даты рождения собаки.
+
+        Аргументы:
+            self: Экземпляр формы.
+
+        Поднимает исключение:
+            ValidationError: Если собака старше 100 лет.
+
+        Возвращает:
+            cleaned_data['birth_date']: Дата рождения собаки.
+        """
         if self.cleaned_data['birth_date']:
             cd = self.cleaned_data['birth_date']
             now_year = datetime.now().year
@@ -28,11 +61,33 @@ class DogForm(StyleFormMixin, forms.ModelForm):
 
 
 class DogAdminForm(StyleFormMixin, forms.ModelForm):
+    """
+    Форма для создания и редактирования информации о собаках в админке.
+
+    Атрибуты:
+        model (Dog): Модель собак.
+        fields (list): Все поля модели доступны для редактирования.
+
+    Методы:
+        clean_birth_date(self): Валидация даты рождения собаки.
+    """
     class Meta:
         model = Dog
         fields = '__all__'
 
     def clean_birth_date(self):
+        """
+        Валидация даты рождения собаки.
+
+        Аргументы:
+            self: Экземпляр формы.
+
+        Поднимает исключение:
+            ValidationError: Если собака старше 100 лет.
+
+        Возвращает:
+            cleaned_data['birth_date']: Дата рождения собаки.
+        """
         if self.cleaned_data['birth_date']:
             cd = self.cleaned_data['birth_date']
             now_year = datetime.now().year
@@ -43,6 +98,13 @@ class DogAdminForm(StyleFormMixin, forms.ModelForm):
 
 
 class ParentForm(StyleFormMixin, forms.ModelForm):
+    """
+    Форма для редактирования родительской модели.
+
+    Атрибуты:
+        model (Parent): Модель родителя.
+        fields (list): Все поля модели доступны для редактирования.
+    """
     class Meta:
         model = Parent
         fields = '__all__'
