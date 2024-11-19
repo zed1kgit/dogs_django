c6a980425ba83838e252fe33ea496355eb768910
Ilya Seleznev <zedplay.zp@gmail.com>
2024-11-19 23:35:57 +0300

    Added reviews forms docstrings

--- reviews/forms.py
+++ reviews/forms.py
@@ -6,6 +6,20 @@ from dogs.forms import StyleFormMixin
 
 
 class ReviewForm(StyleFormMixin, forms.ModelForm):
+    """
+    Форма для создания и редактирования отзывов.
+
+    Поля:
+        dog (ModelChoiceField): Скрытое поле для выбора собаки, к которой относится отзыв.
+        title (CharField): Заголовок отзыва.
+        content (TextInput): Содержимое отзыва.
+        slug (SlugField): Скрытое поле для уникального идентификатора отзыва.
+
+    Метаданные:
+        model (Review): Модель отзыва.
+        fields (list): Список полей, включаемых в форму.
+    """
+
     dog = forms.ModelChoiceField(queryset=Dog.objects.all(), required=False, widget=forms.HiddenInput())
     title = forms.CharField(max_length=150, label='Заголовок')
     content = forms.TextInput()
@@ -14,4 +28,3 @@ class ReviewForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Review
         fields = ['dog', 'title', 'content', 'slug']
-
