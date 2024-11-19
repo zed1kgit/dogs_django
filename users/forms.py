505062c09c58c30d433e4a916800ff1908cb5585
Ilya Seleznev <zedplay.zp@gmail.com>
2024-11-19 23:34:27 +0300

    Added user forms docstrings

--- users/forms.py
+++ users/forms.py
@@ -8,24 +8,63 @@ from users.validators import validate_password
 
 
 class StyleFormMixin:
+    """
+    Миксин для добавления стиля к полям форм.
+    """
+
     def __init__(self, *args, **kwargs):
+        """
+        Инициализация миксина. Добавляет класс 'form-control' ко всем полям формы.
+
+        Аргументы:
+            args: Позиционные аргументы.
+            kwargs: Именованные аргументы.
+        """
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
 
 
 class UserForm(StyleFormMixin, forms.ModelForm):
+    """
+    Форма для редактирования профиля пользователя.
+
+    Метаданные:
+        model (User): Модель пользователя.
+        fields (tuple): Поля, доступные для редактирования.
+    """
+
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name', 'phone', 'avatar',)
 
 
 class UserRegisterForm(StyleFormMixin, UserCreationForm):
+    """
+    Форма для регистрации нового пользователя.
+
+    Метаданные:
+        model (User): Модель пользователя.
+        fields (tuple): Поля, доступные для заполнения при регистрации.
+
+    Метод:
+        clean_password2(self): Проверка совпадения введенных паролей и их валидность.
+    """
+
     class Meta:
         model = User
         fields = ('email',)
 
     def clean_password2(self):
+        """
+        Проверка совпадения введенных паролей и их валидность.
+
+        Возвращает:
+            str: Повторенный пароль, если он совпадает с первым и проходит валидацию.
+
+        Исключения:
+            ValidationError: Если пароли не совпадают или не проходят валидацию.
+        """
         temp_data = self.cleaned_data
         validate_password(temp_data['password1'])
         if temp_data['password1'] != temp_data['password2']:
@@ -37,17 +76,44 @@ class UserRegisterForm(StyleFormMixin, UserCreationForm):
 
 
 class UserLoginForm(StyleFormMixin, AuthenticationForm):
+    """
+    Форма для аутентификации пользователя.
+    """
     pass
 
 
 class UserUpdateForm(StyleFormMixin, forms.ModelForm):
+    """
+    Форма для обновления информации о пользователе.
+
+    Метаданные:
+        model (User): Модель пользователя.
+        fields (tuple): Поля, доступные для редактирования.
+    """
+
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name', 'phone', 'telegram', 'avatar',)
 
 
 class UserChangePasswordForm(StyleFormMixin, PasswordChangeForm):
+    """
+    Форма для изменения пароля пользователя.
+
+    Метод:
+        clean_new_password2(self): Проверка совпадения новых паролей и их валидность.
+    """
+
     def clean_new_password2(self):
+        """
+        Проверка совпадения новых паролей и их валидность.
+
+        Возвращает:
+            str: Новый пароль, если он совпадает с повторенным и проходит валидацию.
+
+        Исключения:
+            ValidationError: Если новые пароли не совпадают или не проходят валидацию.
+        """
         password1 = self.cleaned_data.get("new_password1")
         password2 = self.cleaned_data.get("new_password2")
         validate_password(password1)
