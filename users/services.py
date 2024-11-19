7eb5845f424f33b7e26507c55e4c2eb66480df13
Ilya Seleznev <zedplay.zp@gmail.com>
2024-11-19 23:30:05 +0300

    Added user services docstrings

--- users/services.py
+++ users/services.py
@@ -3,7 +3,14 @@ from django.core.mail import send_mail
 
 from celery import shared_task
 
+
 def send_register_email(email):
+    """
+    Отправка письма с поздравлением о регистрации.
+
+    Аргументы:
+        email (str): Адрес электронной почты получателя.
+    """
     send_mail(
         subject='Поздравляем с регистрацией',
         message='Вы успешно зарегистрировались!',
@@ -11,7 +18,15 @@ def send_register_email(email):
         recipient_list=[email],
     )
 
+
 def send_new_password(email, new_password):
+    """
+    Отправка письма с новым паролем.
+
+    Аргументы:
+        email (str): Адрес электронной почты получателя.
+        new_password (str): Новый пароль пользователя.
+    """
     send_mail(
         subject='Вы успешно изменили пароль',
         message=f'Ваш новый пароль: {new_password}',
@@ -19,6 +34,13 @@ def send_new_password(email, new_password):
         recipient_list=[email],
     )
 
+
 @shared_task
 def send_register_email_task(email):
+    """
+    Задание Celery для асинхронной отправки письма с поздравлением о регистрации.
+
+    Аргументы:
+        email (str): Адрес электронной почты получателя.
+    """
     return send_register_email(email)
